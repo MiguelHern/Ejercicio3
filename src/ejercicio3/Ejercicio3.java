232e1941d4d415bc7712b7c1b1138fb1240d836b
ROCIO <ROCIO@HERNANDEZ>
2021-04-30 00:04:53 -0500

    Ejercicio 3:)

---
+++ src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio3;
+
+/**
+ *
+ * @author ROCIO
+ */
+public class Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numero1;
+        numero1 = 3;
+        int numero2;
+        numero2 = 7;
+        if(numero1 > numero2)
+        {System.out.println("El número" + numero1 + "es mayor");
+        }else if (numero1 == numero2)
+        {System.out.println("Son iguales");
+        }else{
+            System.out.println("El número " + numero2 + " es mayor");}
+        
+    }
+    
+}
